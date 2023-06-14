312ae46abaf4f7d72580bc94fe1545c074bb10bf
JuliaSchirmac <julia.schirmacher@web.de>
2023-06-14 13:24:07 +0200

    Cleand Code and Documentation

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,64 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
+def assignment(new_list, i, old_list, j):
+    '''
+    Change entry of new_list at position i to entry from old_list at position j
+    '''
+    new_list[i] = old_list[j]  #reassginment
 
 
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
+def merge_sort(list_to_sort_by_merge):
+    '''
+    Sortes the list_to_sort_by_merge with the merge-sort algorithm recursively
+    '''
 
-        mergeSort(left)
-        mergeSort(right)
+    if (len(list_to_sort_by_merge) > 1):
+        mid_position = len(list_to_sort_by_merge) // 2 #compute middle position (with rounding)
+        left_list = list_to_sort_by_merge[:mid_position] #left half of list_to_sort_by_merge
+        right_list = list_to_sort_by_merge[mid_position:] #right half of list_to_sort_by_merge
 
-        l = 0
-        r = 0
-        i = 0
+        #sort left and right half recursively
+        merge_sort(left_list) #recursive function call for left half
+        merge_sort(right_list) #recursive function call for right half
 
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
+        #set indices
+        left_index = 0  #index for left_list
+        right_index = 0     #index for right_list
+        new_index = 0   #index for 'new' list (i.e. overwriting of list_to_sort_by_merge)
 
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
+        #if left and right index has not reached end of the list respectively
+        while left_index < len(left_list) and right_index < len(right_list):
+    new_index += 1
 
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
+        #above while ready -> one list (left_list or right_lift) 'finished' ie. index at most right mid_position
+        #check what list index not at the end and fill entries of new list up with remaining entries
 
+        #left_index not at right position of left_list?
+        while left_index < len(left_list):
+            assignment(list_to_sort_by_merge, new_index, left_list, left_index)
+            left_list += 1
+            new_index += 1
 
+        #right_index not at right position of right_list?
+        while right_index < len(right_list):
+            assignment(list_to_sort_by_merge, new_index, right_list, right_index)
+            right_index += 1
+            new_index += 1
+
+#plot list entries before and after sorting
 import matplotlib.pyplot as plt
 
+#create list
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
+
+#create x_range as range of x-values for plotting
+x_range = range(len(my_list))
+
+#plot
+plt.plot(x_range, my_list)
 plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
+
+#sort via above algotihm
+merge_sort(my_list)
+
+#plot result (sorted list)
+plt.plot(x_range, my_list)
 plt.show()
